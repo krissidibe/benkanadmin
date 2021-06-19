d0e59742eb6451b0a70e6d4caa3b80176645b1bd
Aboubacar Sidiki Sidibé <assowlove@gmail.com>
2021-06-19 22:42:11 +0000

    commit message3

--- app/Http/Controllers/Api/EnqUserController.php
+++ app/Http/Controllers/Api/EnqUserController.php
@@ -13,15 +13,24 @@ class EnqUserController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function login()
+    public function login(Request $request)
     {
         $credentials = request(['email', 'password']);
-        return $credentials;
+
+       
      $enqUser =  EnqUser::where('email', '=', $credentials["email"])->where('password', '=', $credentials["password"])->first();
-     return [
+     if ($enqUser) {
+        return [
         "status" => 'success',
         "data" => $enqUser
      ];
+     }else{
+          return [
+        "status" => 'false',
+         
+     ];
+     }
+    
     }
     public function index()
     {
